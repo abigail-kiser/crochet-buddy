5bac3d69ed43edb856ae832de3c58d7515260d74
abigail-kiser <abigail_kiser@hotmail.com>
2025-10-20 12:34:14 -0400

    3 html pages and routes

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
-# Importing flask module in the project is mandatory
-# An object of Flask class is our WSGI application.
 from flask import Flask
+from flask import render_template
 
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
 app = Flask(__name__)
 
-# The route() function of the Flask class is a decorator, 
-# which tells the application which URL should call 
-# the associated function.
+
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
-def hello_world():
-    return 'Hello World'
+def library():
+    return render_template("library.html")
+
+
+@app.route('/discover/')
+def discover():
+    return render_template("discover.html")
+
+
+@app.route('/settings/')
+def settings():
+    return render_template("settings.html")
+
+
+
+
+
 
 # main driver function
 if __name__ == '__main__':
